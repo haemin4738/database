ff1e55c60c16ee49675d83d0b80a21ffe7122bac
Song <31n5ang@gail.com>
2023-06-07 20:23:52 +0900

    create ticketMapper

--- byeongcheonAirLine/ByeongcheonAirLineAPI/src/main/java/com/koreatech/byeongcheonairlineapi/mapper/TicketMapper.java
+++ byeongcheonAirLine/ByeongcheonAirLineAPI/src/main/java/com/koreatech/byeongcheonairlineapi/mapper/TicketMapper.java
@@ -1,7 +1,33 @@
 package com.koreatech.byeongcheonairlineapi.mapper;
 
+import com.koreatech.byeongcheonairlineapi.dto.domain.Ticket;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface TicketMapper {
+    @Select("""
+            SELECT *
+            FROM ticket
+            """)
+    List<Ticket> findAll();
+
+    @Select("""
+            SELECT *
+            FROM ticket
+            WHERE id = #{id}
+            """)
+    Ticket findById(int id);
+
+    @Select("""
+            SELECT *
+            FROM ticket t
+            JOIN member m
+            ON m.id = t.memberId
+            WHERE m.account = #{account}
+            """)
+    Ticket findByMember(String account);
 }
---
+++ byeongcheonAirLine/ByeongcheonAirLineAPI/src/main/java/com/koreatech/byeongcheonairlineapi/service/TicketService.java
@@ -0,0 +1,12 @@
+package com.koreatech.byeongcheonairlineapi.service;
+
+import com.koreatech.byeongcheonairlineapi.dto.LoginDto;
+import com.koreatech.byeongcheonairlineapi.dto.domain.Ticket;
+import java.util.List;
+import com.koreatech.byeongcheonairlineapi.mapper.TicketMapper;
+
+public interface TicketService {
+    List<Ticket> findAll();
+    Ticket findByCustomerId(int id);
+    Ticket findByMember(LoginDto loginDto);
+}

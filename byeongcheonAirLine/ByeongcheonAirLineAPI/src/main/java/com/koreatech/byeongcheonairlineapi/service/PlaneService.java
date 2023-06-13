753eb0e54b0a1ba66c89121db9079f921872836f
SungOh98 <tryjso@naver.com>
2023-06-13 13:12:35 +0900

    deleteProjectFor Ignore

--- byeongcheonAirLine/ByeongcheonAirLineAPI/src/main/java/com/koreatech/byeongcheonairlineapi/service/PlaneService.java
+++
@@ -1,8 +0,0 @@
-package com.koreatech.byeongcheonairlineapi.service;
-
-import com.koreatech.byeongcheonairlineapi.dto.domain.Plane;
-
-public interface PlaneService {
-
-    Plane findByFlightId(int flightId);
-}

790b24b5678a0cee7b94992bfed3db30f9839233
SungOh98 <tryjso@naver.com>
2023-06-13 13:17:34 +0900

    Delete Project for Ingore2

--- byeongcheonAirLine/ByeongcheonAirLineAPI/src/main/java/com/koreatech/byeongcheonairlineapi/dto/LoginDto.java
+++
@@ -1,9 +0,0 @@
-package com.koreatech.byeongcheonairlineapi.dto;
-
-import lombok.Data;
-
-@Data
-public class LoginDto {
-    private String account;
-    private String password;
-}
--- byeongcheonAirLine/ByeongcheonAirLineAPI/src/main/java/com/koreatech/byeongcheonairlineapi/exception/UnAuthorizeException.java
+++
@@ -1,8 +0,0 @@
-package com.koreatech.byeongcheonairlineapi.exception;
-
-public class UnAuthorizeException extends RuntimeException{
-    private static final long serialVersionUID = -2238030302650813813L;
-    public UnAuthorizeException() {
-        super("access-token 만료!, access token을 재발급 받으세요");
-    }
-}
